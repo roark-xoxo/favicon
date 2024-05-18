a39c1cc428f6ce0bee783f733c498ce05ebb5051
marcellocurto <marcello@roark.at>
2024-05-18 20:43:00 +0200

    add basic font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,17 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {},
-  plugins: [],
+	content: [
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Jersey 25"', "Georgia", "serif"],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
