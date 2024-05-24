104b3bf408b47eb6c28ccf00690b0ac5174135ae
marcellocurto <marcello@roark.at>
2024-05-24 09:50:49 +0200

    simplify

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,7 @@
 export function Header() {
 	return (
 		<div className="pb-3 pr-3">
-			<header className="flex items-center justify-between space-x-4 rounded-3xl bg-teal-400/65 px-6 pb-2 pt-4 leading-tight dark:bg-slate-900">
-				<div className="space-y-1">
-					<h1 className="text-4xl leading-6 text-zinc-900 dark:text-zinc-100">
-						Create your favicon
-					</h1>
-					<p className="text-2xl leading-none text-zinc-800 dark:text-zinc-300">
-						It is free and fun.
-					</p>
-				</div>
+			<header className="flex items-center justify-end space-x-4 rounded-3xl bg-teal-400/65 px-2 py-2 leading-tight dark:bg-slate-900">
 				<div>
 					<button className="rounded-full border border-zinc-950 bg-zinc-50 px-6 py-2 uppercase hover:bg-yellow-300">
 						Download
