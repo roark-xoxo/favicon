babb346664b58bd5c604f6d71e0f5657bb9f8af6
marcellocurto <marcello@roark.at>
2024-05-25 15:42:23 +0200

    open in new window

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,7 @@ export function Footer() {
 	return (
 		<footer className="mt-auto flex">
 			<a
+				target="_blank"
 				href="https://roark.at"
 				className="flex flex-col items-center justify-center space-y-2"
 			>
