1d24619ca5a9f8dbd356985f968c492e6948ec2a
marcellocurto <marcello@roark.at>
2024-05-25 14:01:04 +0200

    add mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
+import withMDX from "@next/mdx";
+
+const mdxConfig = withMDX({
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+	},
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	output: "export",
+	experimental: {
+		mdxRs: true,
+	},
 };
 
-export default nextConfig;
+export default mdxConfig(nextConfig);
