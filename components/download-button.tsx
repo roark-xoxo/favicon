ca08611aa66b5b82d02cf15d13b1cbe607d90eaf
marcellocurto <marcello@roark.at>
2024-05-27 17:40:56 +0200

    add more icon sizes

--- components/download-button.tsx
+++ components/download-button.tsx
@@ -5,6 +5,54 @@ import JSZip from "jszip";
 import { saveAs } from "file-saver";
 
 export function DownloadButton() {
+	const iconSizes = [16, 32, 96, 192, 512];
+	const appleTouchIconSizes = [57, 72, 114, 144, 180];
+	const createIcons = async (
+		pngDataUrl: string
+	): Promise<Record<string, string>> => {
+		const icons: Record<string, string> = {};
+
+		for (const size of iconSizes) {
+			const icon = await createIcon(pngDataUrl, size, size);
+			icons[`favicon-${size}x${size}.png`] = icon;
+		}
+
+		for (const size of appleTouchIconSizes) {
+			const icon = await createIcon(pngDataUrl, size, size);
+			icons[`apple-touch-icon-${size}x${size}.png`] = icon;
+		}
+
+		const defaultAppleTouchIcon = await createIcon(pngDataUrl, 180, 180);
+		icons["apple-touch-icon.png"] = defaultAppleTouchIcon;
+
+		return icons;
+	};
+
+	const createIcon = (
+		pngDataUrl: string,
+		width: number,
+		height: number
+	): Promise<string> => {
+		const img = new Image();
+		img.src = pngDataUrl;
+
+		return new Promise((resolve, reject) => {
+			img.onload = () => {
+				const canvas = document.createElement("canvas");
+				const context = canvas.getContext("2d");
+				if (!context) {
+					reject(new Error("Failed to get 2D context"));
+					return;
+				}
+				canvas.width = width;
+				canvas.height = height;
+				context.drawImage(img, 0, 0, width, height);
+				resolve(canvas.toDataURL("image/png").split(",")[1]);
+			};
+			img.onerror = (error) => reject(error);
+		});
+	};
+
 	const createIco = async (pngDataUrl: string): Promise<ArrayBuffer> => {
 		const img = new Image();
 		img.src = pngDataUrl;
@@ -41,11 +89,7 @@ export function DownloadButton() {
 		const iconDirEntrySize = 16;
 
 		const icoHeader = new Uint8Array(fileHeaderSize + iconDirEntrySize);
-
-		// ICO header
 		icoHeader.set([0x00, 0x00, 0x01, 0x00, 0x01, 0x00], 0);
-
-		// Icon directory entry
 		const iconDirEntry = [
 			0x20, // Width: 32
 			0x20, // Height: 32
@@ -79,9 +123,13 @@ export function DownloadButton() {
 			const pngDataUrl = await domToPng(faviconEl);
 			const icoBuffer = await createIco(pngDataUrl);
 
+			const icons = await createIcons(pngDataUrl);
+
 			const zip = new JSZip();
-			zip.file("favicon.png", pngDataUrl.split(",")[1], { base64: true });
 			zip.file("favicon.ico", icoBuffer);
+			for (const [name, data] of Object.entries(icons)) {
+				zip.file(name, data, { base64: true });
+			}
 
 			zip.generateAsync({ type: "blob" }).then((blob) => {
 				saveAs(blob, "favicons.zip");
@@ -94,7 +142,7 @@ export function DownloadButton() {
 	return (
 		<button
 			onClick={downloadFiles}
-			className="rounded-full border border-zinc-950 bg-zinc-50 px-6 py-2 leading-none uppercase hover:bg-yellow-300"
+			className="rounded-full border border-zinc-950 bg-zinc-50 px-6 py-2 uppercase leading-none hover:bg-yellow-300"
 		>
 			Download
 		</button>
